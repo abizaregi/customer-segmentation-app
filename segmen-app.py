cc3c7ae13287de2e406ba430af7d7850b6f88e3e
Abizar Egi <90840338+abizaregi@users.noreply.github.com>
2022-02-05 02:24:19 +0700

    Update segmen-app.py

--- segmen-app.py
+++ segmen-app.py
@@ -34,7 +34,7 @@ if submit:
         def filedownload(df):
             csv = df.to_csv(index=False)
             b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-            href = f'<a href="data:file/csv;base64,{b64}" download="data-app.csv">Download CSV File</a>'
+            href = f'<a href="data:file/csv;base64,{b64}" download="data_app.csv">Download CSV File</a>'
             return href
         st.markdown(filedownload(df_selected), unsafe_allow_html=True)
         
